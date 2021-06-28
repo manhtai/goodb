86ce0a4032578418d80c2ec7729a26f82e55d2d4
manhtai <vo@manhtai.com>
2021-06-28 20:15:03 +0700

    Update index planner

--- plan/opt/update_planner.go
+++ plan/opt/update_planner.go
@@ -1,6 +1,7 @@
 package opt
 
 import (
+	"goodb/index"
 	"goodb/metadata"
 	"goodb/parse"
 	"goodb/plan"
@@ -43,17 +44,68 @@ func (planner *IndexUpdatePlanner) ExecuteInsert(stmt parse.InsertStatement, tx 
 }
 
 func (planner *IndexUpdatePlanner)ExecuteUpdate(stmt parse.UpdateStatement, tx *tx.Transaction) int {
-	panic("Implement me!")
+	tblName := stmt.TableName
+	fldName := stmt.FieldName
+
+	tablePlan := plan.NewTablePlan(tx, tblName, planner.metadataMgr)
+	modifyPlan := plan.NewModifyPlan(tablePlan, stmt.Predicate)
+	modifyScan := modifyPlan.OpenToUpdate()
+
+	indexes := planner.metadataMgr.GetIndexInfo(tblName, tx)
+	indexInfo := indexes[fldName]
+	var idx index.Index
+	if indexInfo != nil {
+		idx = indexInfo.Open()
+	}
+
+	count := 0
+	for modifyScan.Next() {
+		oldVal := modifyScan.GetVal(fldName)
+		val := stmt.Expression.Eval(modifyScan)
+		modifyScan.SetVal(stmt.FieldName, val)
+		count++
+
+
+		if idx != nil {
+			rcd := modifyScan.GetRecord()
+			idx.Delete(oldVal, rcd);
+			idx.Insert(val, rcd);
+		}
+	}
+
+	modifyScan.Close()
+	return count
 }
 
 func (planner *IndexUpdatePlanner)ExecuteDelete(stmt parse.DeleteStatement, tx *tx.Transaction) int{
-	panic("Implement me!")
+	tblName := stmt.TableName
+	tablePlan := plan.NewTablePlan(tx, tblName, planner.metadataMgr)
+	modifyPlan := plan.NewModifyPlan(tablePlan, stmt.Predicate)
+	modifyScan := modifyPlan.OpenToUpdate()
+
+	indexes := planner.metadataMgr.GetIndexInfo(tblName, tx)
+
+	count := 0
+	for modifyScan.Next() {
+		record := modifyScan.GetRecord()
+		for fldName := range indexes {
+			val := modifyScan.GetVal(fldName)
+			idx := indexes[fldName].Open()
+			idx.Delete(val, record)
+			idx.Close()
+		}
+		modifyScan.Delete()
+		count++
+	}
+	return count
 }
 
 func (planner *IndexUpdatePlanner)ExecuteCreateTable(stmt parse.CreateTableStatement, tx *tx.Transaction) int {
-	panic("Implement me!")
+	planner.metadataMgr.CreateTable(stmt.TableName, stmt.Schema, tx)
+	return 0
 }
 
 func (planner *IndexUpdatePlanner)ExecuteCreateIndex(stmt parse.CreateIndexStatement, tx *tx.Transaction) int {
-	panic("Implement me!")
+	planner.metadataMgr.CreateIndex(stmt.IndexName, stmt.TableName, stmt.FieldName, tx)
+	return 0
 }
